fa6de7608712172f34d5ff761139146add055a90
drulac <drulac@protonmail.com>
2020-10-16 23:17:53 +0200

    review

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const fs = require('fs'),
-	templateLiteral = require('template-literal')
+const fs = require('fs')
+const path = require('path')
+
+const templateLiteral = require('../template-literal')
 
 const cache = {}
 
-module.exports = (filePath, options, callback) =>
-	filePath in cache
-		? callback(null, cache[filePath](options))
-		: fs.readFile(
-				filePath,
-				(err, content) =>
-					err
-						? callback(err)
-						: callback(null, (cache[filePath] = templateLiteral(content.toString()))(options))
+const include = (filePath, options = {}, callback) => {
+	if (!(filePath in cache)) {
+		try {
+			filePath = path.resolve(
+				options.settings.views,
+				filePath
 			)
+
+			const content = fs
+				.readFileSync(filePath, 'utf-8')
+				.toString()
+
+			cache[filePath] = templateLiteral(content.toString())
+		} catch (err) {
+			if (callback) callback(err)
+			else throw err
+		}
+	}
+
+	const out = cache[filePath](
+		Object.assign(options, {
+			yugh: 'test',
+			include,
+		})
+	) //to allow include
+
+	if (callback) callback(null, out)
+	else return out
+}
+
+module.exports = include
